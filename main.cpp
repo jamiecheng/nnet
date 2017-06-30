438997ac14dd3222e9e97ab855ba6fc6878ab565
jamie <jamiecheng9@gmail.com>
2017-07-01 00:29:36 +0200

    performance improvements

--- main.cpp
+++ main.cpp
@@ -1,65 +1,55 @@
 #include <iostream>
 
 //#include "net.hpp"
-#include "matrix.hpp"
+#include "array.hpp"
 
 using namespace std;
 
-Matrix nonLin(Matrix x, bool deriv = false) {
+Array nonLin(const Array &x, bool deriv = false) {
     if (deriv) return (1 - x) * x;
 
-    return 1 / (1 + Matrix::exp(-1 * x));
+    return 1 / (1 + Array::exp(-1 * x));
 }
 
 int main() {
-//    NN neuralNetwork(2);
-//
-//    neuralNetwork.addLayer(3, SIGMOID);
-//    neuralNetwork.addLayer(1, SIGMOID);
-//
-//    neuralNetwork.train({{1, 1}, {1, 0}},
-//                        {1, 0}, 1);
-//
-//    auto predicted = neuralNetwork.predict({1, 1});
+    const Array X({{0, 0, 1},
+                   {0, 1, 1},
+                   {1, 0, 1},
+                   {1, 1, 1}});
 
-    const Matrix X({{0, 0, 1},
-                    {0, 1, 1},
-                    {1, 0, 1},
-                    {1, 1, 1}});
+    const Array Y({{0},
+                   {1},
+                   {1},
+                   {0}});
 
-    const Matrix Y({{0},
-                    {1},
-                    {1},
-                    {0}});
-
-    auto syn0 = -1 + (2 * Matrix::randomize(Matrix(3, 4), 0, 1));
-    auto syn1 = -1 + (2 * Matrix::randomize(Matrix(4, 1), 0, 1));
+    auto syn0 = -1 + (2 * Array::randomize(Array(3, 4), 0, 1));
+    auto syn1 = -1 + (2 * Array::randomize(Array(4, 1), 0, 1));
 
     for (int i = 0; i < 10000; i++) {
         auto k0 = X;
-        auto k1 = nonLin(Matrix::dot(k0, syn0));
-        auto k2 = nonLin(Matrix::dot(k1, syn1));
+        auto k1 = nonLin(Array::dot(k0, syn0));
+        auto k2 = nonLin(Array::dot(k1, syn1));
 
         auto k2_error = Y - k2;
 
-        if(i % 2000 == 0) cout << Matrix::mean(k2_error) << endl;
+        if (i % 2000 == 0) cout << Array::mean(k2_error) << endl;
 
         auto k2_delta = k2_error * nonLin(k2, true);
 
-        auto k1_error = Matrix::dot(k2_delta, syn1.transpons());
+        auto k1_error = Array::dot(k2_delta, syn1.transpons());
 
         auto k1_delta = k1_error * nonLin(k1, true);
 
-        syn1 = syn1 + Matrix::dot(k1.transpons(), k2_delta);
-        syn0 = syn0 + Matrix::dot(k0.transpons(), k1_delta);
+        syn1 = syn1 + Array::dot(k1.transpons(), k2_delta);
+        syn0 = syn0 + Array::dot(k0.transpons(), k1_delta);
     }
 
     // predict
-    auto k0 = Matrix({{0, 1, 1}});
-    auto k1 = nonLin(Matrix::dot(k0, syn0));
-    auto k2 = nonLin(Matrix::dot(k1, syn1));
+    auto k0 = Array({{1, 0, 1}});
+    auto k1 = nonLin(Array::dot(k0, syn0));
+    auto k2 = nonLin(Array::dot(k1, syn1));
 
-    cout << k2 << endl;
+    cout << "predicted: " << Array::mean(k2) << endl;
 
     return 0;
 }
